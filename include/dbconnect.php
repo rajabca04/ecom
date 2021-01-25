e19e53455b3c2025ff239bf8965faea719234dd3
rajabca04 <rajabca201804@gmail.com>
2021-01-25 17:10:48 +0530

    25.01.2021

--- include/dbconnect.php
+++ include/dbconnect.php
@@ -1,3 +1,15 @@
 <?php 
     $connect = mysqli_connect('localhost','root','','ecom');
+
+    function count_data($query){
+        global $connect;
+
+        $q = mysqli_query($connect,$query);
+        $count = mysqli_num_rows($q);
+
+        return $count; 
+
+    }
+
+
 ?>
